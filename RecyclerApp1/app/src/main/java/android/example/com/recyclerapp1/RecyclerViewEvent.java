500effb1852998a791d547cb8448f5f49c972b52
MohamedKhaled999 <68979528+MohamedKhaled999@users.noreply.github.com>
2020-08-09 18:33:40 +0200

    Android

--- RecyclerApp1/app/src/main/java/android/example/com/recyclerapp1/RecyclerViewEvent.java
+++
@@ -1,8 +0,0 @@
-package android.example.com.recyclerapp1;
-
-public interface RecyclerViewEvent {
-    void onRecyclerViewClick(int position);
-    void update(int position);
-    void delete(int position);
-
-}
